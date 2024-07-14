2d67115c587b7a1cb1faf4d6f9bb4adab1ecad52
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-07-15 06:23:54 +0700

    feat: add states seeder

--- prisma/seeders/index.ts
+++ prisma/seeders/index.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import countries from "./countries";
+import states from "./states";
 
 const prisma = new PrismaClient();
 
 (async function () {
   try {
     await countries(prisma);
+    await states(prisma);
   } catch (e) {
     console.error(e);
     process.exit(1);
