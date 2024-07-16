c78e069b5d7ab4855ec7411b3737672983202188
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-07-16 09:09:05 +0700

    add open-api query params example

--- src/cities/schema.ts
+++ src/cities/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const QueryCitySchema = z.object({
-  countryId: z.string().min(1),
-  stateId: z.string().min(1),
+  countryId: z.string().min(1).openapi({ example: "102" }),
+  stateId: z.string().min(1).openapi({ example: "1829" }),
   search: z.string().optional(),
 })
--- src/states/schema.ts
+++ src/states/schema.ts
@@ -1,6 +1,6 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const QueryStateSchema = z.object({
-  countryId: z.string().min(1),
+  countryId: z.string().min(1).openapi({ example: "102" }),
   search: z.string().optional(),
-});
+})
