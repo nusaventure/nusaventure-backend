c3346fffa420bc25722c8e6bc008cd91243f6aae
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-07-18 21:49:12 +0700

    feat: update the seeder data

--- prisma/seeders/places.ts
+++ prisma/seeders/places.ts
@@ -8,9 +8,10 @@ export default async function run(prisma: PrismaClient) {
   const places: Array<PlaceSeeder> = [
     {
       countryId: "102",
+      islandId: "clyrb3jup00098vfrpaei32yf",
       stateId: "1825",
       cityId: "56731",
-      categorySlugs: ["localgovernmentoffice"],
+      categorySlugs: ["localgovernmentoffice", "historicalsites"],
       title: "Gedung Sate",
       slug: "gedung-sate",
       description:
@@ -22,6 +23,8 @@ export default async function run(prisma: PrismaClient) {
       zoom: 14,
       address:
         "Jl. Diponegoro No.22, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115",
+      isFeatured: true,
+      position: 3,
     },
     {
       countryId: "102",
@@ -38,6 +41,85 @@ export default async function run(prisma: PrismaClient) {
       zoom: 14,
       address:
         "Jl. Cimincrang No.14, Cimenerang, Kec. Gedebage, Kota Bandung, Jawa Barat 40292",
+    },
+    {
+      countryId: "102",
+      islandId: "clyrb3jup00098vfrpaei32yf",
+      stateId: "1829",
+      cityId: "56816",
+      categorySlugs: ["historicalsites"],
+      title: "Tugu Yogyakarta",
+      slug: "tugu-yogyakarta",
+      description:
+        "White-&-gold landmark on a traffic island, dating from 1889 & floodlit after dark.",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Yogyakarta_Indonesia_Tugu-Yogyakarta-01.jpg/800px-Yogyakarta_Indonesia_Tugu-Yogyakarta-01.jpg",
+      latitude: -7.7829749,
+      longitude: 110.3665674,
+      zoom: 19,
+      address:
+        "Jl. Jend. Sudirman, Gowongan, Kec. Jetis, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55233",
+      isFeatured: true,
+      position: 1,
+    },
+    {
+      countryId: "102",
+      islandId: "clyrb3jus000h8vfrd9x6acc5",
+      stateId: "1826",
+      cityId: "57013",
+      categorySlugs: ["historicalsites"],
+      title: "Tanah Lot",
+      slug: "tanah-lot",
+      description:
+        "Traditional Balinese temple located on a small, rocky island & offering water vistas.",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2015/10/19/20/29/tanah-lot-996675_1280.jpg",
+      latitude: -8.6212003,
+      longitude: 115.0867082,
+      zoom: 20,
+      address: "Beraban, Kec. Kediri, Kabupaten Tabanan, Bali 82121",
+      isFeatured: true,
+      position: 2,
+    },
+    {
+      countryId: "102",
+      islandId: "clyrb3jus000h8vfrd9x6acc5",
+      stateId: "1805",
+      cityId: "56723",
+      categorySlugs: ["historicalsites"],
+      title: "Bundaran HI",
+      slug: "bundaran-hi",
+      description:
+        "The Selamat Datang Monument is located in the center of a roundabout known as Bundaran Hotel Indonesia or Bundaran HI (Indonesian for Hotel Indonesia Roundabout)",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Jakarta_Bundaran_HI_.jpg/1200px-Jakarta_Bundaran_HI_.jpg",
+      latitude: -6.1949851,
+      longitude: 106.8221597,
+      zoom: 18,
+      address:
+        "Menteng, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta",
+      isFeatured: true,
+      position: 4,
+    },
+    {
+      countryId: "102",
+      islandId: "clyrb3jus000h8vfrd9x6acc5",
+      stateId: "1827",
+      cityId: "56803",
+      categorySlugs: ["historicalsites"],
+      title: "Surabaya",
+      slug: "surabaya",
+      description:
+        "Landmark statue symbolic of Surabaya city, featuring a shark & crocodile tussling amid seaweed.",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Jakarta_Bundaran_HI_.jpg/1200px-Jakarta_Bundaran_HI_.jpg",
+      latitude: -7.2960492,
+      longitude: 112.7387012,
+      zoom: 18,
+      address:
+        "Jl. Diponegoro No.1-B, Darmo, Kec. Wonokromo, Surabaya, Jawa Timur 60241",
+      isFeatured: true,
+      position: 5,
     },
   ];
 
