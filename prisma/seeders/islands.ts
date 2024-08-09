99302469799e41ada25b66786ed3dba39b317828
Josa <pratamajosa1234@gmail.com>
2024-08-09 22:24:44 +0700

    fix: Reupload all Island photos to Uploadcare

--- prisma/seeders/islands.ts
+++ prisma/seeders/islands.ts
@@ -9,7 +9,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Papua",
       slug: "papua",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/c/cd/Jembatan_Youtefa_2.jpg",
+        "https://ucarecdn.com/bcc123b6-71a7-4e9b-89a2-8fc6c51a2a59/-/preview/400x300/",
       latitude: -5.5011831,
       longitude: 135.6045112,
       zoom: 7,
@@ -19,7 +19,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Kalimantan",
       slug: "kalimantan",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Banjarmasin_floating_market%2C_2018.jpg/1280px-Banjarmasin_floating_market%2C_2018.jpg",
+        "https://ucarecdn.com/189810e1-facc-4d54-8fb4-f4a7c89673bb/-/preview/400x300/",
       latitude: 1.423257,
       longitude: 108.7552974,
       zoom: 7,
@@ -29,7 +29,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Sumatera",
       slug: "sumatera",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Pagi_hari_di_Danau_Ranau.jpg/1920px-Pagi_hari_di_Danau_Ranau.jpg",
+        "https://ucarecdn.com/dc66eeb2-fabb-4c1b-99b2-d6b59eab25f8/-/preview/400x300/",
       latitude: -0.1427302,
       longitude: 95.3453979,
       zoom: 7,
@@ -39,7 +39,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Sulawesi",
       slug: "sulawesi",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Floating_houses_on_Lake_Tempe.jpg/1280px-Floating_houses_on_Lake_Tempe.jpg",
+        "https://ucarecdn.com/0a6c8e46-7ad9-460b-9ccc-2c1ed3a29adf/-/preview/400x300/",
       latitude: -2.0213952,
       longitude: 120.0933559,
       zoom: 7,
@@ -49,7 +49,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Jawa",
       slug: "jawa",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Baturraden_-_Purwokerto%2C_2015-03-23.jpg/1280px-Baturraden_-_Purwokerto%2C_2015-03-23.jpg",
+        "https://ucarecdn.com/c7c7d68c-d7f1-430d-9356-b8afaed967d8/-/preview/400x300/",
       latitude: -7.3204811,
       longitude: 107.2654557,
       zoom: 8,
@@ -59,7 +59,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Timor",
       slug: "timor",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Timor.A2002179.0205.500m.jpg/1280px-Timor.A2002179.0205.500m.jpg",
+        "https://ucarecdn.com/69d843cd-8a6b-41fd-947d-c37ff132a90d/-/preview/400x300/",
       latitude: -9.6247682,
       longitude: 123.8622907,
       zoom: 9,
@@ -69,7 +69,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Halmahera",
       slug: "halmahera",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Kantor_Bupati_Halmahera_Utara.jpg/1920px-Kantor_Bupati_Halmahera_Utara.jpg",
+        "https://ucarecdn.com/db422a3f-2d62-4314-8813-86e7fc8d19b8/-/preview/400x300/",
       latitude: 0.7191459,
       longitude: 127.5665118,
       zoom: 8,
@@ -79,7 +79,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Seram",
       slug: "seram",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d8/Pearl_farm_%28Seram%2C_Indonesia%29.jpg",
+        "https://ucarecdn.com/85155fc5-994d-4f8f-b74b-c0549bb3fc66/-/preview/400x300/",
       latitude: -3.5633212,
       longitude: 128.5620037,
       zoom: 8.5,
@@ -89,7 +89,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Sumbawa",
       slug: "sumbawa",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Landscape_view_with_Sengeang_volcano_in_Sumbawa%2C_Indonesia%3B_January_2020.jpg/1920px-Landscape_view_with_Sengeang_volcano_in_Sumbawa%2C_Indonesia%3B_January_2020.jpg",
+        "https://ucarecdn.com/cf8d6c09-7479-4362-a030-353f2201e19f/-/preview/400x300/",
       latitude: -8.5942753,
       longitude: 117.2943635,
       zoom: 9,
@@ -99,7 +99,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Flores",
       slug: "flores",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Padar_Island.jpg/1280px-Padar_Island.jpg",
+        "https://ucarecdn.com/71260e12-f9e8-4229-aa7b-a015ddfac784/-/preview/400x300/",
       latitude: -8.5130992,
       longitude: 120.6411681,
       zoom: 8.75,
@@ -109,7 +109,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Kolepom",
       slug: "kolepom",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Tugu_Libra_969_Merauke.jpg/1920px-Tugu_Libra_969_Merauke.jpg",
+        "https://ucarecdn.com/72ca08cb-d3ad-4f8b-b8ba-b6a91ca57a11/-/preview/400x300/",
       latitude: -7.813822,
       longitude: 138.0547589,
       zoom: 9,
@@ -119,7 +119,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Bangka",
       slug: "bangka",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Lake_Shuji_-_Muara_Enim%2C_SS_%284%29.jpg/1920px-Lake_Shuji_-_Muara_Enim%2C_SS_%284%29.jpg",
+        "https://ucarecdn.com/14893116-9e64-46b0-8bc4-ed997b0992db/-/preview/400x300/",
       latitude: -2.2704017,
       longitude: 105.5526031,
       zoom: 9,
@@ -129,7 +129,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Sumba",
       slug: "sumba",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Wikiearth-Taman_Nasional_Kelimutu.jpg/1280px-Wikiearth-Taman_Nasional_Kelimutu.jpg",
+        "https://ucarecdn.com/160eea14-d650-4dbd-820f-b68d4518d2db/-/preview/400x300/",
       latitude: -9.8364167,
       longitude: 119.4446342,
       zoom: 9,
@@ -139,7 +139,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Buru",
       slug: "buru",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/9/97/Wisata-pantai-yanain-di-maluku-tengah.jpg",
+        "https://ucarecdn.com/2804d175-4337-413f-bc89-d34a891fc1ee/-/preview/400x300/",
       latitude: -3.4589616,
       longitude: 126.3023783,
       zoom: 10,
@@ -149,7 +149,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Bali",
       slug: "bali",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Pura_Ulun_Danu_Bratan%2C_2022.jpg/1280px-Pura_Ulun_Danu_Bratan%2C_2022.jpg",
+        "https://ucarecdn.com/f1824ea5-f2b0-4a14-ace4-79d3fe0b32cd/-/preview/400x300/",
       latitude: -8.4676699,
       longitude: 114.7115128,
       zoom: 9,
@@ -159,7 +159,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Nias",
       slug: "nias",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/2/20/Nias_Regent_office.png",
+        "https://ucarecdn.com/57be51b4-e70f-4da0-a63e-a44332c6ec34/-/preview/400x300/",
       latitude: 1.1036798,
       longitude: 97.3705768,
       zoom: 9,
@@ -169,7 +169,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Belitung",
       slug: "belitung",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Pantai_Tanjung_Tinggi%2C_Belitung.jpg/375px-Pantai_Tanjung_Tinggi%2C_Belitung.jpg",
+        "https://ucarecdn.com/61b340ea-3951-41ec-9613-f33e8a3bdf4e/-/preview/400x300/",
       latitude: -2.8993819,
       longitude: 97.3705768,
       zoom: 9,
@@ -179,7 +179,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Buton",
       slug: "buton",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Reflection_of_Floating_House_of_Bajau_in_Bajau_Sampela_Village_Wakatobi.jpg/1280px-Reflection_of_Floating_House_of_Bajau_in_Bajau_Sampela_Village_Wakatobi.jpg",
+        "https://ucarecdn.com/08c975df-3676-4b7c-aecf-4b8506bc5061/-/preview/400x300/",
       latitude: -5.0405425,
       longitude: 122.8116263,
       zoom: 9,
@@ -189,7 +189,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Lombok",
       slug: "lombok",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Lombok_In_Memories%2C_Senggigi.jpg/1280px-Lombok_In_Memories%2C_Senggigi.jpg",
+        "https://ucarecdn.com/33bbc0fc-a1b3-4942-90d4-332f300c4f6f/-/preview/400x300/",
       latitude: -8.5476683,
       longitude: 116.1053826,
       zoom: 9,
@@ -199,7 +199,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Madura",
       slug: "madura",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Gua_Lebar_-_panoramio.jpg/1280px-Gua_Lebar_-_panoramio.jpg",
+        "https://ucarecdn.com/8799b834-b32e-463f-95b7-eebdc50de337/-/preview/400x300/",
       latitude: -7.0581924,
       longitude: 112.7396031,
       zoom: 9,
@@ -209,7 +209,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Nusa Penida",
       slug: "nusa-penida",
       imageUrl:
-        "https://ucarecdn.com/9585c4dc-c428-44ab-a456-78eae617d018/-/preview/750x1000/",
+        "https://ucarecdn.com/1034283d-87a8-4a69-aa1e-46fc5b4e2f39/-/preview/400x300/",
       latitude: -8.7454859,
       longitude: 115.4964409,
       zoom: 9,
@@ -219,7 +219,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Ambon",
       slug: "ambon",
       imageUrl:
-        "https://potretmaluku.id/wp-content/uploads/2021/03/IMG_20210313_193428.jpg",
+        "https://ucarecdn.com/6b44b741-8b3c-4696-b876-775a1a872844/-/preview/400x300/",
       latitude: -3.6421134,
       longitude: 128.0548957,
       zoom: 9,
@@ -229,7 +229,7 @@ export default async function run(prisma: PrismaClient) {
       name: "Komodo",
       slug: "komodo",
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/e/e3/Komodo_Island_north_aerial.jpg",
+        "https://ucarecdn.com/bcbd83c6-8d13-434c-8df6-4529c9a4bf9d/-/preview/400x300/",
       latitude: -8.5888579,
       longitude: 119.3078264,
       zoom: 9,
